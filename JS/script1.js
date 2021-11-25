65e110e36afeee400f752ea19318a523790389a8
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-11-25 18:23:32 +0200

    git

---
+++ JS/script1.js
@@ -0,0 +1,11 @@
+// 1.Напишите функцию deepEqual чтоб она проверяла что два объекта идентичны. Пример:
+
+// function deepEqual(obj1, obj2) {
+//   return JSON.stringify(obj1) === JSON.stringify(obj2);
+// }
+// console.log(deepEqual({ name: "test" }, { name: "test" })); // true
+// console.log(deepEqual({ name: "test" }, { name: "test1" })); //  false
+// console.log(deepEqual({ name: "test" }, { name: "test", age: 10 })); //  false
+
+
+container { width: 200px; height: 200px; border: 1px solid black; item ( width: 58px; height: 50px; background-color: blue; <dlv classa container div
