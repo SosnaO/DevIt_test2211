187d8acd86acd36b1741fc2a8a19923125d6d7eb
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-11-27 20:55:14 +0200

    Js6

--- JS/script5.js
+++ JS/script5.js
@@ -1,4 +1,5 @@
-// 5. Есть функция primitiveMultiply, которая умножает числа, но случайным образом может выбрасывать исключения типа: NotificationException, ErrorException. Задача написать функцию обертку которая будет повторять вычисление при исключении NotificationException, но прекращать работу при исключениях ErrorException
+// 5. Есть функция primitiveMultiply, которая умножает числа, но случайным образом может выбрасывать исключения типа: NotificationException, ErrorException.
+// Задача написать функцию обертку которая будет повторять вычисление при исключении NotificationException, но прекращать работу при исключениях ErrorException
 // Пример:
 // function NotificationException() {}
 // function ErrorException() {}
@@ -17,3 +18,34 @@
 //   // Ваш код
 // }
 // console.log(reliableMultiply(8, 8));
+
+function NotificationException() {}
+function ErrorException() {}
+function primitiveMultiply(a, b) {
+  const rand = Math.random();
+  if (rand < 0.5) {
+    return a * b;
+  } else if (rand > 0.85) {
+    throw new ErrorException();
+  } else {
+    throw new NotificationException();
+  }
+}
+console.log(primitiveMultiply(8, 8));
+
+function reliableMultiply(a, b) {
+  try {
+    throw NotificationException(); // создание исключения
+  } catch (e) {
+    // инструкции для обработки ошибок
+    ErrorException(); // передать объект исключения обработчику ошибок
+  }
+
+  // try {
+  //   NotificationException();
+
+  // } catch {
+  //   ErrorException();
+  // }
+}
+console.log(reliableMultiply(8, 8));
--- JS/script6.js
+++ JS/script6.js
@@ -1,9 +1,9 @@
-// 6. Дана матрица (двумерный массив), нужно написать функцию, которая будет находить наименьшее значение, 
+// 6. Дана матрица (двумерный массив), нужно написать функцию, которая будет находить наименьшее значение,
 //   после чего все нечетные значения в матрице будет умножать на это число.
 
 // Исходная матрица:
 // [
-//  [5, 3, 6], 
+//  [5, 3, 6],
 //  [7, 11, 2],
 //  [15, 9, 4]
 // ]
@@ -13,4 +13,31 @@
 //  [10, 6, 6],
 //  [14, 22, 2],
 //  [30, 18, 4]
+
 // ]
+
+var arr = [
+  [5, 3, 6],
+  [7, 11, 2],
+  [15, 9, 4],
+];
+function oddNumbers(arr) {
+  let out_array = [];
+  let result = [];
+  arr.forEach(function (i) {
+    Array.prototype.push.apply(out_array, i);
+  });
+  const min = Math.min.apply(null, out_array);
+  for (let x = 0; x < arr.length; x++) {
+    array1 = arr[x].slice();
+    result.push(array1);
+    for (let i = 0; i < array1.length; i++) {
+      let item = array1[i];
+      if (item % 2) {
+        array1[i] = item * min;
+      }
+    }
+  }
+  console.log(result);
+}
+oddNumbers(arr);
