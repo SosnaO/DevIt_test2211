92ac7f6cacc0b852bc05f66f87bd9dd85de8cda1
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-11-27 11:26:24 +0200

    LINUX 123 567

--- JS/script1.js
+++ JS/script1.js
@@ -1,17 +1,18 @@
 // 1. Напишите функцию nodeChildCount которая получает на вход объект типа Node и возвращает число всех вложенных нодов,
 // аргумент deep указывать глубину подсчета если не указан то бесконечно.
 //     Пример:
-
 // const div = document.createElement("div");
 // const p = document.createElement("p");
 // const span = document.createElement("span");
 // p.appendChild(span);
 // div.appendChild(p);
+// nodeChildCount(div) // 2
+// nodeChildCount(div, 1) // 1
+// nodeChildCount(div, 2) // 2
 
-var nodeChildCount = function (node, deep = -1) {
-  var result = 0;
-
-  for (var i = 0; i < node.childNodes.length; i++) {
+let nodeChildCount = function (node, deep = -1) {
+  let result = 0;
+  for (let i = 0; i < node.childNodes.length; i++) {
     result = result + 1;
     let elem = node.childNodes[i];
     if (elem.childNodes.length > 0) {
@@ -30,5 +31,6 @@ const p = document.createElement("p");
 const span = document.createElement("span");
 p.appendChild(span);
 div.appendChild(p);
-
 console.log(nodeChildCount(div));
+console.log(nodeChildCount(div, 1));
+console.log(nodeChildCount(div, 2));
--- JS/script2.js
+++ JS/script2.js
@@ -4,9 +4,8 @@
 // iterator.next(); // { value: [1,2,3], done: false }
 // iterator.next(); // { value: [4,5,6], done: false }
 // iterator.next(); // { value: [7,8], done: false }
-// console.log(iterator.next()); // { value: undefined, done: true }
+// iterator.next() // { value: undefined, done: true }
 
-var result = chunkArray([1, 2, 3, 4, 5, 6, 7, 8], 3);
 function chunkArray(array, i) {
   let array_ = array.splice(0, array.length + 1);
   let result = [];
@@ -14,16 +13,14 @@ function chunkArray(array, i) {
     next: function () {
       result = array_.slice(0, i);
       array_ = array_.slice(i);
-      return i == result.length
+      return i === result.length
         ? { value: result, done: false }
         : { value: result, done: true };
     },
   };
 }
-
-var it = chunkArray([1, 2, 3, 4, 5, 6, 7, 8], 3);
-
-console.log("1", it.next()); // 'yo'
-console.log("2", it.next()); // 'ya'
-console.log("3", it.next());
-// console.log(it.next().done); // true
+const iterator = chunkArray([1, 2, 3, 4, 5, 6, 7, 8], 3);
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
